9b5df5d46f424a7fc9a01637db2e28a164cac750
Paul Morrison <paul.unr@gmail.com>
2023-08-30 15:17:29 -0400

    less messy version of eslint implementation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -54,7 +54,10 @@ return {
         -- commented out to prevent duplicate formatting issues with volar
         -- eslint, to match linting rules for nuxt3
         eslint = {
-          settings = {},
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
         },
       },
       -- you can do any additional lsp server setup here
@@ -62,13 +65,23 @@ return {
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
         eslint = function()
-          require("lazyvim.util").on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              -- commenting this out, so I can disable volar autoformat, but still eslint fix
+              -- if not require("lazyvim.plugins.lsp.format").enabled() then
+              --   -- exit early if autoformat is not enabled
+              --   return
+              -- end
+
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end,
+          })
         end,
         -- example to setup with typescript.nvim
         -- tsserver = function(_, opts)
