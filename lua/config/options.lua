d8747cd69df3757343ca46047d7f23f9c60f964d
Paul Morrison <paul.unr@gmail.com>
2024-06-10 20:17:31 -0400

    attempt to blindly update lazyvim (but broke it) (#7)
    * attempt to blindly update lazyvim
    
    * It's mostly working, but sadly not perfect
    
    * upgrade works, has errors
    
    * upgrade works, has errors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.expandtab = false
+
+-- Prevent autoformat from LazyFormat
+-- This is part of my journey to get eslint working on save
+-- vim.g.autoformat = false -- globally
+-- vim.b.autoformat = false -- buffer-local
